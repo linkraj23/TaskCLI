29348f093c17863fbba65576d121a2d70546fec3
Link_raj23 <raj.kautep71@gmail.com>
2025-03-17 21:30:05 +0530

    latest

--- main.cpp
+++ main.cpp
@@ -1,17 +1,48 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
-
-void addTask() {
-    
-    cout << "Enter the task You want to do: " << endl;
-    cin >> x;
-
-}
 int main()
 {
-    cout << "Welcome to the TaskCLI" << endl;
-    addTask();
-    
+    int a = 0;
+    bool validInput = false;
+    while (!validInput)
+    {
+
+        cout << "welcome to task manager app\n";
+        cout << "What do you want to do of the following: \n";
+        cout << "1.add task 2.delete task 3.manage task\n";
+        cout << "4.see completed task 5.see unfinished task\n";
+        cout << "6.see the task that are in progress" << endl;
+        cin >> a;
+        
+    }
+        
+        switch (a)
+        {
+            case 1:
+            cout << "add" << endl;
+            break;
+            case 2: 
+            cout << "delete" << endl;
+    break;
+    case 3: 
+    cout << "manage task" << endl;
+    break;
+    case 4: 
+    cout << "completed task" << endl;
+    break;
+    case 5: 
+    cout << "unfinished task" << endl;
+    break;
+    case 6: 
+    cout << "Task that are in progress" << endl;
+    break;
+    default:
+    cout << "Choose the right option" << endl;
+    break;
+}
+
     return 0;
 }
