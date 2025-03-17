74524676fd62a50093374017ec13437959914fc8
Link_raj23 <raj.kautep71@gmail.com>
2025-03-17 21:28:51 +0530

    latest

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 using namespace std;
 
+
+void addTask() {
+    
+    cout << "Enter the task You want to do: " << endl;
+    cin >> x;
+
+}
 int main()
 {
-    cout << "Hello World" << endl;
+    cout << "Welcome to the TaskCLI" << endl;
+    addTask();
+    
+    return 0;
 }
